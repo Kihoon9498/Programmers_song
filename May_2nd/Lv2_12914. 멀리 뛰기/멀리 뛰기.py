d61a786c4468aa162243059db70a670c954dbe12
Kihoon9498 <kh2929korea@hanmail.net>
2023-05-12 10:12:32 +0900

    Add files via upload

--- May_2nd/Lv2_12914. 멀리 뛰기/멀리 뛰기.py
+++ May_2nd/Lv2_12914. 멀리 뛰기/멀리 뛰기.py
@@ -3,6 +3,8 @@ import math
 def solution(n):
     answer = 0
     for i in range(n):
+        # math 라이브러리의 comb함수를 이용하여 계산함
+        # 다른 사람의 풀이를 보니 피보나치 수열로 계산하는 과정도 있었음
         answer += math.comb(n-i, i)
         if n-i <= i:
             break
--- May_2nd/Lv2_138476. 귤 고르기/귤 고르기.py
+++ May_2nd/Lv2_138476. 귤 고르기/귤 고르기.py
@@ -1,12 +1,13 @@
 def solution(k, tangerine):
     answer = 0
     temp = {}
+    # set으로 중복을 제거하여 count한 값들을 저장하려 하였으나 시간 초과로 문제가 발생하여 하나씩 확인하는 방법으로 변경함
     for i in tangerine:
         if i in temp:
             temp[i] = temp[i] + 1
         else:
             temp[i] = 1
-
+    # 딕셔너리의 values를 리스트로 변경하고 정렬하여 값을 계산
     temp = list(temp.values())
     temp.sort(reverse=True)
     while k > 0:
